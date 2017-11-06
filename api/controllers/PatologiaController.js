252eb00730f6cb27b9094147a0df24bf50b6c704
brablawaxplagabrwabla <gtroncone@correo.unimet.edu.ve>
2017-11-05 21:25:45 -0400

    Falta solo el query de frecuencia

--- api/controllers/PatologiaController.js
+++ api/controllers/PatologiaController.js
@@ -6,6 +6,36 @@
  */
 
 module.exports = {
-	
+
+  index: function(req, res) {
+
+    Patologia.find().exec(function(err, patologia){
+      if (err) sails.log(patologia);
+      res.view({
+        patologia: patologia
+      });
+    });
+  },
+
+  nuevo: function(req, res) {
+    res.view();
+  },
+
+  new: function(req, res) {
+    Patologia.create(req.params.all()).exec(function(err, patologia){
+      if (err) sails.log(err);
+      res.redirect('back');
+    });
+  },
+
+  delete: function(req, res) {
+    Describe.destroy({patologia: req.param('id')}).exec(function(err, describe){
+      if (err) sails.log(err);
+      Patologia.destroy({id: req.param('id')}).exec(function(err, patologia){
+        res.redirect('back');
+      });
+    });
+  }
+
 };
 
